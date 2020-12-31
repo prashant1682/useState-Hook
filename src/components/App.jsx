ffc6845b5118aa69cd7e07befb8a739092e1332c
prashant1682 <prashant009@gmail.com>
2020-12-31 09:55:10 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function App() {
-  return <div />;
+  // useState fucntion has two values in the array , one object and one fucntion .
+  // by assigning a const in the array on LHS we are assigning a value to the array in RHS.
+  // NOw when count is called it will retrun the object value =0
+  // setCount will call the fucntion
+  //Once the app loads , the count value 0 will be shown.
+  // When the + button is clicked , it will call the increase fucntion and add 1 to the count and return setCount fucntion.
+  // The count object will get the value from setCount fucntion and refresh the page
+  const [count, setCount] = React.useState(0);
+
+  function increase() {
+    // the setCount fucntion will add 1 to the object
+    // setCount fucntion helps to update the value of count
+    return setCount(count + 1);
+  }
+
+  function decrease() {
+    // the setCount fucntion will reduce 1 from the object
+    // setCount fucntion helps to update the value of count
+    return setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
   <div className="container">
-    <h1>0</h1>
-    <button>+</button>
+    <App />
   </div>,
   document.getElementById("root")
 );
